ca8e853394a7da736496488d83c4e22492d3c601
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-02 17:24:38 +0530

    Initial Commit packages and links added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBarLeft from './Components/SideBar/SideBarLeft'
+
+const App = () => {
+  return (
+    <div className='w-full bg-(--primary-color) min-h-screen p-4 text-white'>
+      Hello Drameoo OTT Platform!
+    </div>
+  )
+}
+
+export default App
