6d42a0c21fe773fa9cda7b43ddbc2325a98a7202
SubastralAlok <alokkumaryadav241004@gmail.com>
2026-01-05 13:38:13 +0530

    Defined sidebar data

---
+++ src/data/sidebarData.js
@@ -0,0 +1,24 @@
+import {
+  Home,
+  Compass,
+  Layers,
+  Heart,
+  Play,
+  Clock,
+  FolderOpen,
+  Download,
+} from "lucide-react";
+
+export const menuItems = [
+  { icon: Home, label: "Home", active: true },
+  { icon: Compass, label: "Explore" },
+  { icon: Layers, label: "Genres" },
+  { icon: Heart, label: "Favourites" },
+];
+
+export const libraryItems = [
+  { icon: Play, label: "Continue Watching" },
+  { icon: Clock, label: "Recently Added" },
+  { icon: FolderOpen, label: "My Collections" },
+  { icon: Download, label: "Downloads" },
+];
